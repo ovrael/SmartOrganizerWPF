1cef895760d58980e5ff8c54bd6791a079d4e072
ovrael <ovrael@gmail.com>
2023-11-10 02:03:03 +0100

    Added icons to organized tree added drag and drop to organized tree

--- Common/Trees/OrganizedTree.cs
+++ Common/Trees/OrganizedTree.cs
@@ -1,11 +1,18 @@
-﻿using System.IO;
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+
 
 namespace SmartOrganizerWPF.Common.Trees
 {
     public class OrganizedTree
     {
         private readonly TreeView treeView;
+        private TreeViewItem? fromDragDirectory = null;
 
         public OrganizedTree(TreeView treeView)
         {
@@ -14,6 +21,7 @@ namespace SmartOrganizerWPF.Common.Trees
 
         public void BuildTree(string[] organizedFiles)
         {
+            treeView.Items.Clear();
             for (int i = 0; i < organizedFiles.Length; i++)
             {
                 string[] parts = organizedFiles[i].Split('?');
@@ -32,8 +40,9 @@ namespace SmartOrganizerWPF.Common.Trees
             foreach (var item in items)
             {
                 if (item is not TreeViewItem treeItem) continue;
-                if (treeItem.Header is not string header) continue;
-                if (header == directoryName) return treeItem;
+                if (treeItem.Header is not StackPanel header) continue;
+                if (header.Tag is not string headerTag) continue;
+                if (headerTag == directoryName) return treeItem;
             }
 
             return searchItem;
@@ -55,28 +64,108 @@ namespace SmartOrganizerWPF.Common.Trees
                 return;
             }
 
-            TreeViewItem directoryToAdd = RetrieveTreeItem(organizedPath, items);
-            directoryToAdd ??= AddDirectoryTreeItem(organizedPath, items);
+            TreeViewItem directoryParent = RetrieveTreeItem(organizedPath, items);
+            directoryParent ??= AddDirectoryTreeItem(organizedPath, items);
 
-            TreeViewItem fileTreeItem = new TreeViewItem()
-            {
-                Header = Path.GetFileName(filePath),
-                Tag = filePath
-            };
-
-            directoryToAdd.Items.Add(fileTreeItem);
+            TreeViewItem fileTreeItem = CreateFileTreeItem(filePath);
+            directoryParent.Items.Add(fileTreeItem);
         }
 
-        private TreeViewItem AddDirectoryTreeItem(string name, ItemCollection collectionToAdd)
+        private TreeViewItem AddDirectoryTreeItem(string name, ItemCollection childrens)
         {
-            TreeViewItem directory = new TreeViewItem()
-            {
-                Header = name
-            };
+            TreeViewItem directory = CreateDirectoryTreeItem(name);
 
-            collectionToAdd.Add(directory);
+            childrens.Add(directory);
 
             return directory;
+        }
+
+        private TreeViewItem CreateDirectoryTreeItem(string directoryName)
+        {
+            StackPanel itemHeader = new StackPanel() { Orientation = Orientation.Horizontal, Tag = directoryName };
+
+            // Folder icon
+            Uri uri = new Uri(Tools.ResourcesPath + "/Images/folder_icon_closed.png");
+            BitmapImage bitmap = new BitmapImage(uri);
+            System.Windows.Controls.Image image = new System.Windows.Controls.Image()
+            {
+                Source = bitmap,
+                Width = 18,
+                Height = 18,
+                Margin = new System.Windows.Thickness(5, 0, 0, 0)
+            };
+            itemHeader.Children.Add(image);
+
+            // Direcotry name
+            Label label = new Label() { Content = directoryName };
+            itemHeader.Children.Add(label);
+
+
+            TreeViewItem directory = new TreeViewItem() { Header = itemHeader, AllowDrop = true };
+            directory.Drop += Directory_Drop;
+
+            return directory;
+        }
+
+        private void Directory_Drop(object sender, DragEventArgs e)
+        {
+            if (sender is not TreeViewItem directoryItem) return;
+            e.Handled = true;
+
+            if (e.Data.GetData(DataFormats.StringFormat) is not string combinedData) return;
+
+            string[] data = combinedData.Split('?');
+            int.TryParse(data[0], out int index);
+
+            TreeViewItem movedTreeItem = CreateFileTreeItem(data[1]);
+            directoryItem.Items.Add(movedTreeItem);
+
+            if (fromDragDirectory != null)
+            {
+                fromDragDirectory.Items.RemoveAt(index);
+                fromDragDirectory = null;
+            }
+        }
+
+        private TreeViewItem CreateFileTreeItem(string filePath)
+        {
+            StackPanel itemHeader = new StackPanel() { Orientation = Orientation.Horizontal, Tag = filePath };
+
+            // File icon
+            System.Windows.Controls.Image image = new System.Windows.Controls.Image()
+            {
+                Source = IconManager.FindIconForFilename(filePath),
+                Width = 18,
+                Height = 18,
+                Margin = new Thickness(5, 0, 0, 0)
+            };
+            itemHeader.Children.Add(image);
+
+            // File name
+            Label label = new Label() { Content = Path.GetFileName(filePath) };
+            itemHeader.Children.Add(label);
+
+            TreeViewItem file = new TreeViewItem() { Header = itemHeader };
+
+            file.MouseMove += FileTreeItemDragStart_MouseMove;
+
+            return file;
+        }
+
+        private void FileTreeItemDragStart_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (sender is not TreeViewItem fileTreeItem) return;
+            if (!fileTreeItem.IsSelected) return;
+            if (e.LeftButton != MouseButtonState.Pressed) return;
+
+            if (fileTreeItem.Header is not StackPanel header) return;
+            if (header.Tag is not string filePath) return;
+
+            if (fileTreeItem.Parent is not TreeViewItem parent) return;
+            fromDragDirectory = parent;
+            int itemIndex = fromDragDirectory.Items.IndexOf(fileTreeItem);
+
+            DragDrop.DoDragDrop(fileTreeItem, $"{itemIndex}?{filePath}", DragDropEffects.Copy);
         }
 
         public void MoveFilesOnDisk(string mainDirectory)
